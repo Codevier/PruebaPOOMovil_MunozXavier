169c8de515570b3d3e3433fe4094287ec701eefc
Xaavier <xavier.munoz.pisco@gmail.com>
2020-07-23 17:59:06 -0500

    Probando libreria

--- app/src/main/java/com/example/pruebapoomovil_munozxavier/MainActivity.java
+++ app/src/main/java/com/example/pruebapoomovil_munozxavier/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mindorks.placeholderview.PlaceHolderView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private PlaceHolderView mGalleryView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
+        mGalleryView
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)))
+                .addView(new GalleryItem(getResources().getDrawable(R.drawable.embuandes_opt)));
+
     }
 }
